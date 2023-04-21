7de69640d13d681a69f5764c8a57c4b8b5480ea2
Julivito22 <julietagimenez_6@hotmail.com>
2023-04-21 10:01:59 -0300

    PI POKEMON

--- Downloads/PI-Pokemon-main (2)/PI-Pokemon-main/client/src/componentes/HomePage.jsx
+++ Downloads/PI-Pokemon-main (2)/PI-Pokemon-main/client/src/componentes/HomePage.jsx
@@ -1,18 +1,28 @@
-import React, { Fragment } from "react";
+import React from "react";
 import {useState, useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import { getPokemons } from "../actions";
 import { Link } from "react-router-dom";
 import Card from "./Card";
+import Paginado from "./Paginado";
 
 export default function Home (){
 
     const dispatch = useDispatch()
     const allPokemons = useSelector((state) => state.pokemons)
+    const [currentPage, setCurrentPage] = useState(1)
+    const [pokemonsPerPage, setPokemonsPerPage] = useState(12)
+    const indexOfLastPokemon = currentPage * pokemonsPerPage
+    const indexOfFirstPokemon = indexOfLastPokemon - pokemonsPerPage
+    const currentPokemons = allPokemons.slice(indexOfFirstPokemon,indexOfLastPokemon)
+
+    const paginado = (pageNumber) => {
+        setCurrentPage(pageNumber)
+    }
 
     useEffect (()=>{
         dispatch(getPokemons());
-    },[])
+    },[dispatch])
 
     function handleClick(e){
         e.preventDefault();
@@ -37,7 +47,11 @@ export default function Home (){
                     <option value='created'>Creados</option>
                     <option value='api'>De la api</option>
                 </select>
-                {allPokemons?.map((el) =>{
+                <Paginado pokemonsPerPage={pokemonsPerPage}
+                allPokemons={allPokemons.length}
+                paginado = {paginado}
+                />
+                {currentPokemons?.map((el) =>{
                     return (
                         <fragment>
                         <Link to={"/home/" + el.id}>
